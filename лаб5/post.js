12c86678e21af09bebee57c7f33cf05d970673b3
Denis <but.dan-but201@yandex.ru>
2022-10-06 20:24:40 +0300

    Create post.js

---
+++ лаб5/post.js
@@ -0,0 +1,10 @@
+let form = document.querySelector('form_post'),
+    formInput = document.querySelectorAll('form_input'),
+    inputName = document.querySelector('form_input_name'),
+    inputGroup = document.querySelector('form_input_group'),
+    inputShot = document.querySelector('form_input_shot'),
+    textareaText = document.querySelector('form_input_text');
+
+    form.onsubmit( () =>{
+
+    })
