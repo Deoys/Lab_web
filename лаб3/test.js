aa520ab0f4942c6c9a844233fb3134cf4127d31e
Denis <but.dan-but201@yandex.ru>
2022-10-06 11:31:53 +0300

    Update test.js

--- лаб3/test.js
+++ лаб3/test.js
@@ -1,9 +1,9 @@
 let button_on = document.getElementById('on');
 let button_off = document.getElementById('off');
-
 let list = document.querySelectorAll('.iteams_katalog');
+let color = window.getComputedStyle( document.body ,null).getPropertyValue('background-color');
 
-console.log(list)
+console.log(color)
 
 
 button_on.addEventListener('click', function(){
@@ -11,6 +11,16 @@ button_on.addEventListener('click', function(){
     for(let i = 0; i<list.length; i++){
         if(i % 2!= 0){
             list[i].style.backgroundColor = "red";
+            console.log(list[i]);
+        }
+    }    
+})
+
+button_off.addEventListener('click', function(){
+
+    for(let i = 0; i<list.length; i++){
+        if(i % 2!= 0){
+            list[i].style.backgroundColor = "color";
             console.log(list[i]);
         }
     }    
