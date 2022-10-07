5a5bff6665bca534eba5b022e0579ecf7ac1caf9
Denis <but.dan-but201@yandex.ru>
2022-10-07 09:40:56 +0300

    Update post.js

--- лаб5/post.js
+++ лаб5/post.js
@@ -1,10 +1,40 @@
-let form = document.querySelector('form_post'),
-    formInput = document.querySelectorAll('form_input'),
-    inputName = document.querySelector('form_input_name'),
-    inputGroup = document.querySelector('form_input_group'),
-    inputShot = document.querySelector('form_input_shot'),
-    textareaText = document.querySelector('form_input_text');
+const Name = document.getElementById("name"),
+      Text = document.getElementById("text"),
+      nameError = document.querySelector('#name + span.error'),
+      form  = document.getElementsByTagName('form')[0];
 
-    form.onsubmit( () =>{
 
-    })
+Name.addEventListener('input', function(event){
+    if(Name.validity.valid){
+        nameError.textContent='';
+        nameError.className='error';
+    }else{
+        showError();
+    }
+})
+
+form.addEventListener('submit', function(event){
+    if(!Name.validity.valid){
+        showError();
+        event.preventDefault();
+    }
+})
+
+function showError() {
+    if(Name.validity.valueMissing) {
+      // Если поле пустое,
+      // отображаем следующее сообщение об ошибке
+      nameError.textContent = 'You need to enter an e-mail address.';
+    } else if(Name.validity.typeMismatch) {
+      // Если поле содержит не email-адрес,
+      // отображаем следующее сообщение об ошибке
+      nameError.textContent = 'Entered value needs to be an e-mail address.';
+    } else if(Name.validity.tooShort) {
+      // Если содержимое слишком короткое,
+      // отображаем следующее сообщение об ошибке
+      nameError.textContent = `Email should be at least ${ Name.maxLength } characters; you entered ${ email.value.length }.`;
+    }
+  
+    // Задаём соответствующую стилизацию
+    nameError.className = 'error active';
+  }
