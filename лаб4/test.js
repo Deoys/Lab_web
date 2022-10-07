816e4de420c87c7e0fb98a6fa0beccef599222b7
Denis <but.dan-but201@yandex.ru>
2022-10-07 09:12:56 +0300

    Update test.js

--- лаб4/test.js
+++ лаб4/test.js
@@ -13,57 +13,14 @@ for (let anchor of anchors){
 }
 
 
-
-//изменение bg четных элементов
-const button = document.getElementById("btn");
-const button_none = document.getElementById("btn_none");
-const iteams = document.querySelectorAll('.iteams_katalog');
-let default_color = window.getComputedStyle( document.body ,null).getPropertyValue('background-color');
-const hex=[
-    "0","1","2","3","4","5","6","7","8","9","A","B","C","D", "E", "F"
-]
-
-
-button_none.addEventListener("click", () =>{
-    for(let n=0; n < iteams.length; n++){
-        if(n % 2 !=0){
-        iteams[n].style.backgroundColor = default_color;
-        }
-    }
-})
-
-button.addEventListener("click", () => {
-    let hexColor = generateHex();
-    for(let n=0; n < iteams.length; n++){
-        if(n % 2 !=0){
-            iteams[n].style.backgroundColor = hexColor;
-            }
-        
-    }
-})
-
-function generateHex(){
-    let hexColor="#";
-    for(let i = 0; i<6; i++){
-        hexColor += hex[getRandomNum()];
-    }
-    return hexColor;
-}
-
-function getRandomNum(){
-    return Math.floor(Math.random()*hex.length);
-}
-
-
-
 //сортировка
 
 let list = document.querySelector('#Katalog')
-let iteam = document.querySelectorAll(".iteams_katalog");
+let iteam = document.getElementsByClassName("iteams_katalog");
 let select = document.getElementById("#sort");
 
 select.addEventListener('change', () =>{
-
+ console.log(iteam);
     if(select.value == "min"){
     let sorted = [...iteam].sort((a,b) =>{
         return a.children[2].innerHTML - b.children[2].innerHTML;
@@ -93,5 +50,49 @@ select.addEventListener('change', () =>{
         console.log(sorted);
 }
 })
+
+
+
+//изменение bg четных элементов
+const button = document.getElementById("btn");
+const button_none = document.getElementById("btn_none");
+let iteams = document.querySelectorAll('.iteams_katalog');
+let default_color = window.getComputedStyle( document.body ,null).getPropertyValue('background-color');
+const hex=[
+    "0","1","2","3","4","5","6","7","8","9","A","B","C","D", "E", "F"
+]
+
+
+button_none.addEventListener("click", () =>{
+    for(let n=0; n < iteam.length; n++){
+        if(n % 2 !=0){
+        iteam[n].style.backgroundColor = default_color;
+        }
+    }
+})
+
+button.addEventListener("click", () => {
+    let hexColor = generateHex();
+    for(let n=0; n < iteam.length; n++){
+        if(n % 2 !=0){
+            console.log(iteam[n]);
+            iteam[n].style.backgroundColor = hexColor;
+            }
+        
+    }
+})
+
+function generateHex(){
+    let hexColor="#";
+    for(let i = 0; i<6; i++){
+        hexColor += hex[getRandomNum()];
+    }
+    return hexColor;
+}
+
+function getRandomNum(){
+    return Math.floor(Math.random()*hex.length);
+}
+
 
 
